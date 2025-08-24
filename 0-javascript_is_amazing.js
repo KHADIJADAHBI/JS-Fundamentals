36734b2b9242f648155e33f9a8da2a0583e07bff
KHADIJA <dedadeda425@gmail.com>
2025-08-24 19:44:01 +0200

    Complete 0-javascript_is_amazing.js and 1-multi_languages.js

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+// Define a constant variable named myVar with the value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+
+// Print the value of myVar to the console
+console.log(myVar);
